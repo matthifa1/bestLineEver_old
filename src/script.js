25fd1489d860f8509cf7a4059edecabd11a9e705
matthifa1 <56159315+matthifa1@users.noreply.github.com>
2021-08-11 11:52:19 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,1 +1,40 @@
+function funKonig() {
+  //alert("Hallo König");
+  
+  // Get the checkbox
+  var checkBox = document.getElementById("checkKonige");
+  // Get the output text
+  var text = document.getElementById("1");
 
+  // If the checkbox is checked, display the output text
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+  
+  var text = document.getElementById("3");
+
+  // If the checkbox is checked, display the output text
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+} 
+
+function funBibelbuch() {
+	
+  //alert("Hallo Bibelbuch");
+  // Get the checkbox
+  var checkBox = document.getElementById("checkBibelbuch");
+  // Get the output text
+  var text = document.getElementById("2");
+
+  // If the checkbox is checked, display the output text
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+} 
